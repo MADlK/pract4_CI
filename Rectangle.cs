3c180ce657a35d29c3830397ee5b5c17c6d4ceb0
Sveta616 <svetamarinina44@gmail.com>
2026-03-10 10:11:27 +0500

    first

---
+++ Rectangle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+    public class Rectangle
+    {
+        private double _width;
+        public double Width 
+        {
+            get => _width;
+            set
+            {
+                if (value < 0.001)
+                    throw new ArgumentException("Ширина не может быть меньше 0,001");
+
+                _width = value;
+            }
+        }
+
+
+        private double _height;
+        public double Height 
+        {
+            get => _height;
+            set
+            {
+                if (value < 0.001)
+                    throw new ArgumentException("Высота не может быть меньше 0,001");
+                
+                _height = value;
+            }
+        }
+
+
+        public double Area ()
+        {
+            return _width * _height;
+        }
+        public double Perimeter ()
+        {
+            return _height + _width;
+        }
+
+        public override string ToString ()
+        {
+            return $"Ширина: {_width}, высота: {_height}, Площадь: {Area()}, Периметер: {Perimeter()}";
+        }
+    }
+}
